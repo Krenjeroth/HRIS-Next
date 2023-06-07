7f18402ae312f67d864c38e2aaca5f97bc35cfbd
PenguSoup <99572768+PenguSoup@users.noreply.github.com>
2023-06-07 10:24:14 +0800

    updated holidays

---
+++ src/app/settings/holidays/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Tabs from './Tabs';
+export const metadata = {
+  title: 'HRIS - Holidays',
+};
+
+
+function page() {
+  return (
+    <div>
+      <Tabs />
+    </div>
+  )
+}
+
+export default page
